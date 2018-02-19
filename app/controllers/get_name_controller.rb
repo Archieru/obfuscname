989691bb8bfa623a8f34980efc4c278d824c4864
Dmitry Pikulkin <dmitry.pikulkin@toptal.com>
2018-02-19 18:57:00 +0200

    use default seed if none given

--- app/controllers/get_name_controller.rb
+++ app/controllers/get_name_controller.rb
@@ -1,12 +1,13 @@
 class GetNameController < ApplicationController
   def index
-    seed = Digest::MD5.hexdigest(params[:seed])
-    name_seed = "0x#{seed[0,15]}".to_i(16)
-    surname_seed = "0x#{seed[16,31]}".to_i(16)
+    seed = params[:seed] || DateTime.now.to_s
+    hash = Digest::MD5.hexdigest(seed)
+    name_seed = "0x#{hash[0,15]}".to_i(16)
+    surname_seed = "0x#{hash[16,31]}".to_i(16)
     name_id = 1 + (name_seed % Name.all.count)
     surname_id = 1 + (surname_seed % Surname.all.count)
     return_name = Name.find(name_id).name
     return_surname = Surname.find(surname_id).surname
-    render json: {status: 'SUCCESS', message: "hello, #{return_name} #{return_surname}"}, status: :ok
+    render json: {seed: seed, name: "#{return_name} #{return_surname}"}, status: :ok
   end
 end
